0917ecd1ddc94ad49bf05519b98e8d3bf97ad4ed
Fernando Lopez <cainuriel@gmail.com>
2024-01-05 22:37:27 +0100

    config hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
 /* global ethers task */
 require('@nomiclabs/hardhat-waffle')
 require("hardhat-gas-reporter");
-
+require("@nomiclabs/hardhat-etherscan");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async () => {
@@ -29,5 +29,16 @@ module.exports = {
   },
   gasReporter: {
     enabled: true
-  }
+  },
+  networks: {
+    goerli: {
+        url: "YOUR_QUICKNODE_HTTP_ENDPOINT",
+        accounts: ["YOUR_PRIVATE_KEY"]
+    }
+    },
+    etherscan: {
+        // Your API key for Etherscan
+        // Obtain one at https://etherscan.io/
+        apiKey: "YOUR_ETHERSCAN_API_KEY"
+    }
 }
